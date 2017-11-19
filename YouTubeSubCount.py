4ec9448ee7c5a47d66a964eb02118102cc570655
mcd223 <jacob@simplelittledream.com>
2017-11-19 16:07:54 -0500

    Update YouTubeSubCount.py

--- YouTubeSubCount.py
+++ YouTubeSubCount.py
@@ -6,7 +6,7 @@ import os
 
 while True:
 
- r = requests.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=CHANNEL_ID&key=YOUTUBE_DATA_API_V3')
+ r = requests.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCXUuGvjW1KLq6oqfEX0BieQ&key=AIzaSyC6xLODu34WFpDHdqaQf8Lfmokh5wYX_is')
  j = r.json()
 
  subs = j['items'][0]['statistics']['subscriberCount']
@@ -18,4 +18,3 @@ while True:
  print "YOUR CHANNEL Channel Stats:"
  print "Total number of subscribers: %s" % (subs)
  print "Total number of views: %s" % (views)
-
